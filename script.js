d88130f2c94f5206811d40431dc6152459f38ae9
WelchsGrape <mywelchsgrape@gmail.com>
2022-05-31 02:21:45 +0900

    test

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-var parseData = JSON.parse(JSON.stringify(Params));
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+readTextFile("https://WelchsGrape.github.io/mystic.bms");
